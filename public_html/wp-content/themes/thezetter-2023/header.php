4eff801d384fa618040dad0f0d985f77e820066b
Josep Rosell <josep@adao.co.uk>
2023-09-19 13:52:15 +0100

    Adding hotels network scripts

--- public_html/wp-content/themes/thezetter-2023/header.php
+++ public_html/wp-content/themes/thezetter-2023/header.php
@@ -68,10 +68,20 @@ dataLayer.push({
 </style>
 <?php if(get_field('not_zetter', 'options') || !get_field('is_group', 'options')): ?>
     <?php include 'secondary_styles.php'; ?>
-    <!-- Hotels Network -->
-    <script src='https://www.thehotelsnetwork.com/js/loader.js?property_id=1042281&account_key=C46AE18617ED62CF0B1CA1E7F6CEFB84' async></script>
     <!-- TripTease -->
     <script src="https://onboard.triptease.io/bootstrap.js?integrationId=01E4X68PPMXP1TVN2VV3FHBD2W" defer async crossorigin="anonymous" type="text/javascript"></script>
+<?php endif; ?>
+
+
+<?php if( get_current_blog_id() == 2 ): ?>
+  <!-- The Hotels Network - Clerkenwell -->
+  <script src='https://www.thehotelsnetwork.com/js/loader.js?property_id=1042280&account_key=C46AE18617ED62CF0B1CA1E7F6CEFB84' async></script>
+<?php elseif( get_current_blog_id() == 3 ): ?>
+  <!-- The Hotels Network - Marrable's   -->
+  <script src='https://www.thehotelsnetwork.com/js/loader.js?property_id=1042281&account_key=C46AE18617ED62CF0B1CA1E7F6CEFB84' async></script>
+<?php elseif( get_current_blog_id() == 4 ): ?>
+  <!-- The Hotels Network - Marylebone -->
+  <script src='https://www.thehotelsnetwork.com/js/loader.js?property_id=1039746&account_key=C46AE18617ED62CF0B1CA1E7F6CEFB84' async></script>
 <?php endif; ?>
 
 <?php if($_SESSION['not_zetter']): ?>
